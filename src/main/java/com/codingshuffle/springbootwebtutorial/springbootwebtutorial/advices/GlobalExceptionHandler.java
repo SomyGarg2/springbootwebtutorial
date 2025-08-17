a5526ca966c0e74b5fa5b04023575b00a137b809
Somy Garg <lovegarg236@gmail.com>
2025-08-17 09:45:06 +0530

    Transforming API response

--- src/main/java/com/codingshuffle/springbootwebtutorial/springbootwebtutorial/advices/GlobalExceptionHandler.java
+++ src/main/java/com/codingshuffle/springbootwebtutorial/springbootwebtutorial/advices/GlobalExceptionHandler.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFoundException exception) {
+    public ResponseEntity<ApiResponse<?>> handleResourceNotFound(ResourceNotFoundException exception) {
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.NOT_FOUND)
                 .message(exception.getMessage())
@@ -23,11 +23,12 @@ public class GlobalExceptionHandler {
                 .timestamp(LocalDateTime.now()) // ✅ fixed field name
                 .build();
 
-        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+        return buildErrorResponseEntity(apiError);
     }
 
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiError> handleInternalServerError(Exception exception) {
+    public ResponseEntity<ApiResponse<?>> handleInternalServerError(Exception exception) {
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .message(exception.getMessage())
@@ -35,11 +36,11 @@ public class GlobalExceptionHandler {
                 .subErrors(null)
                 .build();
 
-        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+        return buildErrorResponseEntity(apiError);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiError> handleInputValidationErrors(MethodArgumentNotValidException exception) {
+    public ResponseEntity<ApiResponse<?>> handleInputValidationErrors(MethodArgumentNotValidException exception) {
         List<String> errors = exception.getBindingResult()
                 .getAllErrors()
                 .stream()
@@ -53,6 +54,10 @@ public class GlobalExceptionHandler {
                 .subErrors(errors) // ✅ list of validation errors
                 .build();
 
-        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+        return buildErrorResponseEntity(apiError);
+    }
+
+    private ResponseEntity<ApiResponse<?>> buildErrorResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(new ApiResponse<>(apiError), apiError.getStatus());
     }
 }
